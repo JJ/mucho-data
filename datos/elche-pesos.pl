d14d3ffe0c18607e4d3e0f8ad540c45a6e16478d
JJ Merelo <jjmerelo@gmail.com>
2015-05-14 09:14:25 +0200

    Ahora con el peso

--- datos/elche-pesos.pl
+++ datos/elche-pesos.pl
@@ -10,18 +10,19 @@ my $url = "http://www.elchecf.es/plantilla";
 
 my $dom = Mojo::DOM->new( get $url );
 
-my $jugadores =  $dom->find("div.journal-content-article tr");
-my %pesos;
+my $jugadores =  $dom->find("div.titulo_datos_comunes");
+my @pesos;
 for my $p ( @$jugadores ) {
-  say $p->content();
-  my $cells = $p->children();
-  next if !$cells->[1];
-  my $numero = $cells->[1]->text();
-  if ( $numero =~ /\d/ ) {
-    my $peso_div = $p->at('div#peso')->content();
-    say $peso_div;
-    my ($peso) = ($peso_div =~ /(\d+)/);
-    $pesos{$numero} = $peso;
+  if ( $p->content() =~ /Peso/ ) {
+    my $peso;
+    if ( $p->content() =~ /div/ ) {
+      ($peso) = ( $p->content() =~ /o: (\d+)/ );
+    } else {
+      ($peso) = ( $p->content() =~ /(\d+)/ );
+    }
+    push @pesos, $peso;
   }
 }
-say join("\n",map("$_;$pesos{$_}", keys %pesos ));
+say("Indice;Peso");
+my $i = 1;
+say join("\n",map($i++.";".$_, @pesos ));
