d27edad24dec4d91131f7e9d502ca7a956bf61d0
JJ Merelo <jjmerelo@gmail.com>
2018-10-12 20:52:48 +0200

    Añade alturas y pesos de jugadores

--- datos/millonarios.R
+++ datos/millonarios.R
@@ -1,3 +1,6 @@
 library(ggplot2)
+library(ggthemes)
 
 datos <- read.csv("millonarios.dat",sep=";")
+ggplot(datos,aes(x=reorder(Nombre,-Peso),y=Peso,color=Posición))+ geom_point()+theme_tufte()+theme(axis.text.x = element_text(angle = 90, hjust = 1))+xlab("Jugador")
+ggplot(datos,aes(x=reorder(Nombre,-Altura),y=Altura,color=Posición))+ geom_point()+theme_tufte()+theme(axis.text.x = element_text(angle = 90, hjust = 1))+xlab("Jugador")
